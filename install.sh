8f0f4d17eb63a6ca8e7c81f10a15d30e3ef15198
Xueliang Zhong <xueliang.zhong@linaro.org>
2016-12-29 21:26:00 +0000

    Add .ctags config

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 rm -f ~/.vimrc
 rm -f ~/.tmux.conf
+rm -f ~/.ctags
+
+###########################################
 
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/vimrc ~/.config/nvim/init.vim
+
 ln -sf `pwd`/tmux.conf ~/.tmux.conf
+
 ln -sf `pwd`/m ~/bin/m
+
+ln -sf `pwd`/ctags ~/.ctags
