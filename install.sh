3c3967b9118313ac30f268d410012f2a6e541a48
Xueliang Zhong <xueliang.zhong@linaro.org>
2016-12-30 20:07:15 +0000

    Refine install.sh.

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
-rm -f ~/.vimrc
-rm -f ~/.tmux.conf
-rm -f ~/.ctags
-
-###########################################
-
+# vim
+rm -f ~/.vimrc ~/.config/nvim/init.vim
 ln -sf `pwd`/vimrc ~/.vimrc
 ln -sf `pwd`/vimrc ~/.config/nvim/init.vim
 
+# tmux
+rm -f ~/.tmux.conf
+rm -f ~/.window.tmux
 ln -sf `pwd`/tmux.conf ~/.tmux.conf
 ln -sf `pwd`/window.tmux ~/.window.tmux
 
+# ctags settings
+rm -f ~/.ctags
+ln -sf `pwd`/ctags ~/.ctags
+
+# m command
+rm -f ~/bin/m
 ln -sf `pwd`/m ~/bin/m
 
-ln -sf `pwd`/ctags ~/.ctags
