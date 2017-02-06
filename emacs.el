24eca6b686a5b0893fb28a690ed89d245030b6c5
Xueliang Zhong <xueliang.zhong@linaro.org>
2017-02-06 20:55:10 +0000

    improve tmux.conf

--- emacs.el
+++ emacs.el
@@ -45,10 +45,10 @@
 (setq evil-mode-line-format 'before)
 (setq helm-for-files-preferred-list
       '(helm-source-buffers-list
-	helm-source-recentf
-	helm-source-bookmarks
-	helm-source-file-cache
-	helm-source-files-in-current-dir))
+        helm-source-recentf
+        helm-source-bookmarks
+        helm-source-file-cache
+        helm-source-files-in-current-dir))
 
 ; highlight like vim, C-x SPC to remove all persistant search highlights.
 ;(require 'highlight)
