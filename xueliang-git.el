b2c2ada4305cc524fa28d28b6de5f38092fb0d57
xueliang.zhong <xueliang.zhong@linaro.org>
2017-02-14 16:17:11 +0000

    improve xueliang-git.el

--- xueliang-git.el
+++ xueliang-git.el
@@ -84,7 +84,11 @@
   "run git rebase on selected top lines in glog.\nRequire $EDITOR to be set properly." (interactive)
   (if (null (buffer-file-name (current-buffer)))
       (funcall (lambda () ;; already in shell command output buffer.
-                 (setq-local rebase-cmd (message "/bin/bash ~/bin/git-rebase-head %d" (count-lines (region-beginning) (region-end))))
+                 (setq-local rebase-cmd
+			     ;; have to write following way because git rebase seems to require a highly functional shell,
+			     ;; which the default emacs shell-command cannot provide.
+			     (message "/bin/bash ~/bin/git-rebase-head %d"
+				      (count-lines (region-beginning) (region-end))))
                  (print rebase-cmd) (async-shell-command rebase-cmd)))
       (funcall (lambda () ;; in some other file.
                  (xueliang-glog) (message "try apply this function in glog.")))))
