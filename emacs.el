bb70d499e02f57c797812998e80b6d7c99c962b1
Xueliang Zhong <xueliang.zhong@linaro.org>
2017-02-15 20:12:25 +0000

    improve emacs.el

--- emacs.el
+++ emacs.el
@@ -384,17 +384,3 @@
 ;; https://github.com/kai2nenobu/guide-key
 ;; http://aaronbedra.com/emacs.d/#sec-1
 ;; https://github.com/noctuid/evil-guide/blob/master/README.org
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
