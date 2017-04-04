0a2a69ed986c96aa6ddf366419c0b1a9fec90c40
xueliang.zhong <xueliang.zhong@linaro.org>
2017-04-04 17:34:17 +0100

    improve emacs.el

--- emacs.el
+++ emacs.el
@@ -191,9 +191,21 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 ;; add dotty support in org-babel
-(org-babel-do-load-languages 'org-babel-load-languages
-                             '((dot . t)
-                               (emacs-lisp . t)))
+(org-babel-do-load-languages (quote org-babel-load-languages)
+    (quote ((emacs-lisp . t)
+            (java . t)
+            (dot . t)
+            (ditaa . t)
+            (R . t)
+            (python . t)
+            (ruby . t)
+            (gnuplot . t)
+            (clojure . t)
+            (sh . t)
+            (ledger . t)
+            (org . t)
+            (plantuml . t)
+            (latex . t))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Company config
