108348dd5ff9b65d955c4d06fbde05ca36c1bfad
xueliang.zhong <xueliang.zhong@linaro.org>
2017-05-09 12:52:28 +0100

    Don't add a time stamp line to the 'DONE' task.

--- emacs.el
+++ emacs.el
@@ -231,8 +231,8 @@
 (defun =============org-config=============())
 (setq org-todo-keywords '((sequence "TODO" "PROGRESS" "ON-GOING-EFFORT" "DONE")))
 
-;; Add a time stamp to the 'DONE' task.
-(setq org-log-done t)
+;; Don't add a time stamp line to the 'DONE' task.
+(setq org-log-done nil)
 
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode -1)))
