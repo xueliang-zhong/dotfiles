4845819dae2afb5c135e2e4594826ed6e8e1ffee
xueliang.zhong <xueliang.zhong@linaro.org>
2017-07-20 15:50:56 +0100

    Small fix for j movement.

--- emacs.el
+++ emacs.el
@@ -114,7 +114,7 @@
 (setq evil-mode-line-format 'before)
 
 ;; Same behavior on last line as vim.
-(define-key evil-normal-state-map (kbd "j") '(lambda() (interactive) (if (< (+ (line-number-at-pos) 1) (line-number-at-pos (point-max)))
+(define-key evil-normal-state-map (kbd "j") '(lambda() (interactive) (if (<= (+ (line-number-at-pos) 1) (line-number-at-pos (point-max)))
                                                                          (evil-next-line)
                                                                          (message "Last line of buffer.")
                                                                        )))
