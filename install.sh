c59c5769f759735d9959d844fe58fab0a96ed432
Xueliang Zhong <xueliang.zhong@gmail.com>
2024-05-11 13:20:13 +0100

    code improvements for Mac OS.

--- install.sh
+++ install.sh
@@ -5,11 +5,15 @@ mkdir -p ~/bin/
 
 # vim
 rm -f ~/.vimrc
-ln -sf `pwd`/vimrc ~/.vimrc
+ln -sf ${PWD}/vimrc ~/.vimrc
 
 # emacs
 rm -f ~/.doom.d/config.el
-ln -sf `pwd`/doom-config.el ~/.doom.d/config.el
+ln -sf ${PWD}/doom-config.el ~/.doom.d/config.el
+
+# tmux
+rm -f ~/.tmux.conf
+ln -sf ${PWD}/tmux.conf ~/.tmux.conf
 
 # DONE
 echo DONE
