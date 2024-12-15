aa7ec1e1bb5604830fa9ec1e63b4267b4d2abfda
Xueliang Zhong <xueliang.zhong@gmail.com>
2024-12-15 19:23:08 +0000

    Introduce doom-config.el

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 # vim
 rm -f ~/.vimrc
-ln -sf ${PWD}/vimrc ~/.vimrc
+ln -sf $PWD/vimrc ~/.vimrc
 
 # emacs
+rm -f ~/.doom.d/init.el
 rm -f ~/.doom.d/config.el
-ln -sf ${PWD}/doom-config.el ~/.doom.d/config.el
+ln -sf $PWD/doom-init.el ~/.doom.d/init.el
+ln -sf $PWD/doom-config.el ~/.doom.d/config.el
 
 # tmux
 rm -f ~/.tmux.conf
-ln -sf ${PWD}/tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux.conf ~/.tmux.conf
 
 # ~/bin
 mkdir -p ~/bin/
-cp -f ${PWD}/links ~/bin/
+cp -f $PWD/links ~/bin/
 
 # DONE
 echo DONE
