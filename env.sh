a777af87caa494da05dec8b20143d2641d06a3bd
Xueliang Zhong <xueliang.zhong@gmail.com>
2024-12-26 20:45:23 +0000

    code improvements for Mac OS.

--- env.sh
+++ env.sh
@@ -33,18 +33,19 @@ cp -f $DOT_FILE_PATH/links ~/bin/
 git config --global user.name "Xueliang Zhong"
 git config --global user.email "xueliang.zhong@gmail.com"
 
-# Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # Fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+# Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #
 # My fzf based quick commands
 #
 alias d='echo $(dirs | sed "s/ /\n/g"| fzf)'
-alias f='vim  $(fzf --preview "cat {}")'
+# alias f='vim  $(fzf --preview "cat {}")'
+alias f='vim  $(fzf --preview "bat {} --style=plain --color=always")'
 alias ff=f
 alias r='eval $(fc -ln 10000 | fzf --no-sort --reverse --height 40%)'
 alias h=r
