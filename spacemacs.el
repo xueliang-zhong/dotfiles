a39f0e3e601683bdaac605e8f8e6e69bbc8d4b77
Xueliang Zhong <xueliang.zhong@gmail.com>
2025-01-16 22:57:20 +0000

    Update spacemacs.el

--- spacemacs.el
+++ spacemacs.el
@@ -540,7 +540,7 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-use-clean-aindent-mode t
 
    ;; Accept SPC as y for prompts if non-nil. (default nil)
-   dotspacemacs-use-SPC-as-y y
+   dotspacemacs-use-SPC-as-y t
 
    ;; If non-nil shift your number row to match the entered keyboard layout
    ;; (only in insert state). Currently supported keyboard layouts are:
