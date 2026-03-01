a85b18596009ccecc02f6de75bdc519a19de1d75
Xueliang Zhong <xueliang.zhong@gmail.com>
2026-03-01 22:28:14 +0000

    Update spacemacs.el

--- spacemacs.el
+++ spacemacs.el
@@ -60,6 +60,7 @@ This function should only modify configuration layer settings."
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
    dotspacemacs-additional-packages '(just-mode
+                                      lua-mode
                                       doom-themes
                                       graphviz-dot-mode
                                       org-superstar
@@ -255,7 +256,7 @@ It should only modify the values of Spacemacs settings."
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
    dotspacemacs-default-font '("JetBrains Mono"
-                               :size 18
+                               :size 18.0
                                :weight regular
                                :width normal)
 
